19dcb15e56f36be2ff5e723cb8418beee90916fb
Eduardo Alfonso Moreno Perez <ealfonsomp@Eduardos-MacBook-Air.local>
2026-06-23 20:08:31 -0600

    Add build logging: console output to build.log and TASM listing to main.lst

--- src/build.bat
+++ src/build.bat
@@ -1,14 +1,22 @@
 @echo off
 echo Building Tank Battalion...
-echo.
-tasm main.asm, ..\build\main.obj
+echo %date% %time% > ..\build\build.log
+echo. >> ..\build\build.log
+echo TASM 4.1 compile... >> ..\build\build.log
+tasm /zi /m2 main.asm, ..\build\main.obj, ..\build\main.lst >> ..\build\build.log 2>&1
 if errorlevel 1 goto error
-tlink ..\build\main.obj, ..\build\main.exe, ..\build\main.map
+echo TASM: OK >> ..\build\build.log
+echo. >> ..\build\build.log
+echo TLINK 7.1 link... >> ..\build\build.log
+tlink /v ..\build\main.obj, ..\build\main.exe, ..\build\main.map >> ..\build\build.log 2>&1
 if errorlevel 1 goto error
-echo.
+echo TLINK: OK >> ..\build\build.log
+echo. >> ..\build\build.log
 echo BUILD SUCCESSFUL: ..\build\main.exe
+echo BUILD SUCCESSFUL >> ..\build\build.log
 goto done
 :error
 echo.
-echo BUILD FAILED
+echo BUILD FAILED - See build\build.log and build\main.lst for details
+echo BUILD FAILED >> ..\build\build.log
 :done
